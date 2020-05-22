f17a961407b4ad8e2889e70e0a58e1f5bef327e2
Gregori Piñeres <gregori.pineres02@gmail.com>
2020-05-22 08:37:33 -0400

    Add migrations and docker config

---
+++ database/migrations/2020_05_22_122100_create_payments_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('uuid')->unique();
+            $table->date('payment_date')->nullable();
+            $table->date('expires_at');
+            $table->enum('status', ['paid', 'pending'])->default('pending');
+            $table->double('clp_usd')->nullable();
+
+            $table->unsignedBigInteger('client_id');
+            $table->foreign('client_id')->references('id')->on('clients');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+}
